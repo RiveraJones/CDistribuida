a91128d6ba270062b949677cdf58747c6fbcfeeb
RiveraJones <oskar_skilled_@live.com>
2021-02-02 02:33:58 -0500

    forwait, critical y no se que ej poner para un nowait donde sea critico

--- Unidad11/programasOpenMP-V1/loopOpenMP.c
+++ Unidad11/programasOpenMP-V1/loopOpenMP.c
@@ -1,26 +1,40 @@
 #include <stdio.h>
 #include <omp.h>
+#include <time.h>
 
 int main(void) {
 
 // shared variable
 int sum_shared = 0;
-#pragma omp parallel
+//#pragma omp parallel
+#pragma omp parallel num_threads(4)
 {
+    int n = omp_get_num_threads();
+    int id  = omp_get_thread_num();
+    printf("\nHilo numero %d de %d hilos usados\n", id, n); // sum is indeterminate here
     // private variables (one for each thread)
     int sum_local = 0;
+    //#pragma omp for nowait
     #pragma omp for nowait
-    //#pragma omp for
     for (int i = 0; i < 10; ++i) {
         sum_local += i;
+        printf("Sum local del hilo %d = %d\n", id, sum_local);
     }
+
+    //actualizacion de datos globales
+    //openMP esperara a que todos los hilos hayan finalizado para entrar en la zona critica
     //#pragma omp critical
-    {
+    //{
+        //printf("Sum local del hilo %d = %d\n", id, sum_local);
+        printf("//%d\n", sum_shared);
         sum_shared += sum_local;
-    }
+        //printf("Sum shared del hilo %d = %d\n", id, sum_shared);
+        printf("%d\n", sum_shared);
+    //}
 }
 //print(sum_shared);
- printf("%d\n", sum_shared); // sum is indeterminate here
+//report the result
+ printf("---------------%d\n\n", sum_shared); // sum is indeterminate here
 
 /* code */
 return 0;
